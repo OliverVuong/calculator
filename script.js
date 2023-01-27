6655058ad382e8b22aa630c60312a7e7212e56ea
Oliver Vuong <vuongoliver@gmail.com>
2023-01-26 17:19:09 -0800

    Add functionality to the number buttons

--- script.js
+++ script.js
@@ -25,3 +25,23 @@ function operate(operator = 'add', a = 0, b = 0){
         return divide(a, b);
     }
 }
+
+////////////////////////////////////////
+
+const display = document.querySelector('.display');
+let displayStr = "";
+let runningTotal = undefined;
+let operator = undefined;
+let startNewNumber = false;
+
+const numberBtns = document.querySelectorAll('.number');
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        if(startNewNumber){
+            displayStr = "";
+            startNewNumber = false;
+        }
+        displayStr += button.id;
+        display.textContent = displayStr;
+    });
+});
